09585307aaeef44a6d67f390da6a7e5966e8b20a
jyotipatil123 <deshjyoti@yahoo.co.in>
2023-06-08 20:00:41 +0530

    c# training dell

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿//// c# basics 
+//using System;
+
+//namespace A
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Console.Write( "welcome to c# --Dell Batch ");  
+//            // Console.Write("welcome again");
+
+//            //// Console.Read();    // enter to exit
+//            // //Console.ReadLine(); // enter to exit 
+//            // Console.ReadKey();  // any key to exit
+
+//            // // camel case, upper case, lower case, sentence case
+//            // // taking the input
+
+//            Console.WriteLine("Enter first number : ");
+//            // data types :  int, char, float, double, string............
+//            int num1 = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine("Enter second number : ");
+//            int num2 = int.Parse(Console.ReadLine());
+
+//            //Console.WriteLine("Addition result is : "+(num1+num2));
+//            //Console.WriteLine("Subtraction result is : " + (num1 - num2));
+//            //Console.WriteLine("Multiplication result is : " + (num1 * num2));
+//            //Console.WriteLine("Division result is : " + (num1 / num2)); //quotient
+//            //Console.WriteLine("Modulus result is : "+(num1%num2)); //remainder
+
+//            //Console.WriteLine( "Enter your name : ");
+//            //string s1 = Console.ReadLine();
+
+//            //Console.WriteLine("Entered name is : "+s1);
+
+//            //operators : 
+//            // arithematic operators   +   -   /   *   %
+//            // assignment operator =
+//            // comparison operator ==
+//            // ternary opeartor ?=
+//            // realtional operators  <  >   <=  >=  !=
+//            // logical operator   &&   ||   !
+
+//            /* ..........................*/
+//            ///
+
+//            //variable declaration 
+//            //int a = 10;
+//            //Console.WriteLine("a value is : "+a);
+//            //a = 20;
+//            //Console.WriteLine("a value is : " + a);
+//            //a = 30;
+//            //Console.WriteLine("a value is : " + a);
+//            //a = 40;
+//            //Console.WriteLine("a value is : " + a);
+
+//            //const declaration
+
+//            //const int b = 10;
+//            //Console.WriteLine("b value is : " + b);
+//            //b = 20;
+//            //Console.WriteLine("b value is : " + b);
+//            //b = 30;
+//            //Console.WriteLine("b value is : " + b);
+//            //b = 40;
+//            //Console.WriteLine("b value is : " + b);
+
+//            //// implicit conversion 
+//            //int a = 189278787;
+//            //long b = a;
+//            //Console.WriteLine("long b="+b);
+
+//            //// explicit conversion 
+//            //double d = 8787778.22343;
+//            //Console.WriteLine("current value of d double : " + d);
+
+//            //int x =(int) d;
+//            //Console.WriteLine("x="+x);
+
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//=========================================================================
+
