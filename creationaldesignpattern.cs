a54720b88f5de75d707b1bf85f35a587d0272882
jyotipatil123 <deshjyoti@yahoo.co.in>
2023-06-21 14:47:21 +0530

    21st June Design Pattern programs
    creational and structural design patterns programs

---
+++ creationaldesignpattern.cs
@@ -0,0 +1,110 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//namespace creational_design_pattern
+//{
+//     class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//           // abstract factory pattern 
+
+//            mykitchen obj = new mykitchen();
+//            obj.createitems();
+
+//            List<kitchenitem> items = obj.getitems();
+
+//            foreach (kitchenitem item in items)
+//            {
+//                Console.WriteLine(item.print());
+//            }
+
+//            // singleton pattern 
+      
+//            thisapplication app1 = thisapplication.Instance();
+//            app1.setname("App1");
+//            Console.WriteLine(app1.getname());
+            
+//            thisapplication app2 = thisapplication.Instance();
+//            app2.setname("App2");
+//            Console.WriteLine(app2.getname());
+
+//            Console.WriteLine(app1.getname()+"       "+ app2.getname());
+
+//            // prototype design pattern
+//            bookingtype seat1 = new booking();
+//            seat1.setSeat("12A");
+//            Console.WriteLine( seat1.getseat());
+
+//            bookingtype seat2 = seat1.clone();
+//            Console.WriteLine(seat2.getseat());
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+//abstract class kitchenitem {   public abstract string print(); }
+//class spoon : kitchenitem {    public override string print()   { return "spoon";   } }
+//class pan : kitchenitem {      public override string print()   { return "pan"; } }
+//class kettle : kitchenitem {   public override string print()  { return ("kettle");  } }
+//class glass : kitchenitem{     public override string print()    {  return ("glass");   }}
+
+//abstract class kitchen
+//{
+//    protected List<kitchenitem> mitems= new List<kitchenitem>();    
+
+//    public List<kitchenitem> getitems()
+//    {
+//        return mitems;  
+//    }
+
+//    public abstract void createitems();
+//}
+
+//class mykitchen : kitchen
+//{
+//    public override void createitems()
+//    {
+//       mitems.Add(new spoon());
+//       mitems.Add(new pan());
+//       mitems.Add(new kettle());
+//    }
+//}
+
+//class thisapplication
+//{
+//    private static thisapplication _instance;
+
+//    private string name = "This application";
+        
+//    public thisapplication()
+//    {   
+//        Console.WriteLine("welcome");
+//    }
+//    public static thisapplication Instance()
+//    {
+//        if (_instance == null)
+//        {
+//            _instance = new thisapplication();
+//        }
+//        return _instance;
+//    }
+//    public string getname() { return name; }
+//    public void setname(string n) { name = n; }
+//}
+
+//public abstract class bookingtype
+//{
+//    private string mseat;
+//    public void setSeat(string s) { mseat = s; }
+//    public abstract bookingtype clone();
+//    public string getseat() {  return mseat; }
+//}
+
+//class booking : bookingtype
+//{
+//    public override bookingtype clone()
+//    {
+//        return this.MemberwiseClone() as bookingtype;
+//    }
+//}
---
+++ structuraldesignpattern.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace creational_design_pattern
+{
+     class structuraldesignpattern
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Adapter design pattern : ");
+
+            india obj = new india(new workabroad());
+            Console.WriteLine(obj.startwork());
+
+            //====================================================================
+
+            Console.WriteLine("Bridge pattern :");
+
+            invoice invoice = new invoice();
+
+            invoice.setcustomer("ICICI Bank");
+            invoice.setid("ABC123");
+
+            invoiceitem item1 = new invoiceitem();
+            item1.setvalues("item 1", 1, 12.5M, 65.35M);
+
+            invoice.additem(item1);
+
+            invoiceitem item2 = new invoiceitem();
+            item2.setvalues("item 2", 10, 14.5M, 75.35M);
+
+            invoice.additem(item2);
+
+            invoiceitem item3 = new invoiceitem();
+            item3.setvalues("item 3", 12, 18.5M, 76.35M);
+
+            invoice.additem(item3);
+
+            Console.WriteLine("Invoice total =" + invoice.gettotal()) ;
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IWorkAbroad   {     string dowork();   }
+    class india
+    {
+        private readonly IWorkAbroad _workabroad;
+        public india(IWorkAbroad work)
+        {
+            _workabroad = work;
+        }
+        public string startwork()
+        {
+            return _workabroad.dowork();
+        }       
+    }
+    class china
+    {
+           public string workhere()
+           {
+               return "you are working in china";
+           }
+    }
+
+    class workabroad : IWorkAbroad
+    {
+        readonly china _china = new china();
+        public string dowork()
+        {
+            return _china.workhere();
+        }
+    }
+
+    abstract class InvoiceItemType
+    {
+        protected string name;
+        protected int qty;
+        protected decimal rate;
+        protected decimal price;
+
+        public abstract decimal getprice();
+        public abstract string getname();
+        public abstract int getqty();
+        public abstract decimal getrate();
+        public abstract void setvalues(string name, int qty, decimal rate, decimal price);
+    }
+
+    class invoiceitem : InvoiceItemType
+    {
+        public override string getname()   {     return name;  }
+        public override decimal getprice() {      return price;  }
+        public override int getqty()       {       return qty;    }
+        public override decimal getrate()   {       return rate; }
+
+        public override void setvalues(string name, int qty, decimal rate, decimal price)
+        {
+            this.name=name;
+            this.qty=qty;
+            this.rate=rate;
+            this.price = price;
+        }
+    }
+
+    abstract class InvoiceType
+    {
+        protected string id;
+        protected string customer;
+        protected List<InvoiceItemType> _items;
+
+        public abstract void additem(InvoiceItemType item);
+        public abstract void setcustomer(string name);
+        public abstract void setid(string id);
+        public abstract List<InvoiceItemType> getItems();
+        public abstract decimal gettotal();
+    }
+
+    class invoice : InvoiceType
+    {
+        public invoice()     {           _items= new List<InvoiceItemType> ();   }
+        public override void additem(InvoiceItemType item)
+        {
+            _items.Add(item);
+        }
+
+        public override List<InvoiceItemType> getItems()
+        {
+            return _items;
+        }
+
+        public override decimal gettotal()
+        {
+            decimal total = 0;
+            foreach (InvoiceItemType i  in _items)
+            {
+                total += i.getprice();
+            }
+            return total;
+        }
+
+        public override void setcustomer(string name)
+        {
+          this.customer= name;  
+        }
+
+        public override void setid(string id)
+        {
+          this.id=id;
+        }
+    }
+
+    interface IBank
+    {
+        string deposit();
+        string withdraw();
+        decimal getbalance();
+    }
+
+    class bank
+    {
+        public string deposit() { return "deposited"; }
+        public string withdraw() { return "withdraw"; }
+        public string getbalance() { return "5000"; }
+
+    }
+
+    class bankproxy
+    {
+        private bank _bank= new bank();
+
+        public string deposit() { return _bank.deposit(); }
+        public string withdraw() { return _bank.withdraw(); }
+        public string getbalance() { return _bank.getbalance(); }
+
+    }
+}
